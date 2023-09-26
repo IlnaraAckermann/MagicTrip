a0661a9e522e5a8eab0cbd90ba0cfa89faa26b49
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-09-26 18:17:31 -0300

    add: DestinoDAO.java

---
+++ src/java/JDBC_connect/src/DAO/DestinoDAO.java
@@ -0,0 +1,22 @@
+package DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import connect.Connect;
+import entity.Destinos;
+
+public class DestinoDAO {
+    public void cadastrarDestino(Destinos destino) {
+        String sql = "INSERT INTO USUARIO ( BOME, DESCRICAO) VALUES (?, ?)";
+        PreparedStatement ps = null;
+        try {
+            ps = Connect.getConnection().prepareStatement(sql);
+            ps.setString(1, destino.getNome());
+            ps.setString(2, destino.getDescricao());
+            ps.execute();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
