caf5139ddaf72c2328b0856bbaa13aa1ec1fff30
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-09-28 22:25:01 -0300

    UPDATE: CRUD TABELA USUARIO, TESTE OK

--- src/java/JDBC_connect/src/App.java
+++ src/java/JDBC_connect/src/App.java
@@ -7,8 +7,7 @@ import querys.ListarTabelas;
 public class App {
     public static void main(String[] args) throws Exception {
 
-
-        //Criando o usuario e adicionando informações
+        // Criando o usuario e adicionando informações
         // Usuario ilnara = new Usuario();
         // ilnara.setCpf("12345678911");
         // ilnara.setLogradouro("Rua da Alegria");
@@ -23,22 +22,82 @@ public class App {
         // ilnara.setNome_ultimo("D Avila");
         // ilnara.setEmail("ilnara.ackermann@hotmail.com");
         // ilnara.setSenha("UmaSenhaQu@lqu3r");
-        //chamando a classe UsuarioDao para poder realizar o create no DB
+        // chamando a classe UsuarioDao para poder realizar o create no DB
         // new UsuarioDAO().cadastrarUsuario(ilnara);
 
-
-        //Criando a classe destino e cadastrando o Destino no BD
+        // Criando a classe destino e cadastrando o Destino no BD
         // Destinos terraDasFadas = new Destinos();
         // terraDasFadas.setNome("Terra das Fadas");
-        // terraDasFadas.setDescricao("Um reino encantado onde fadas dançam entre flores e árvores mágicas.");
+        // terraDasFadas.setDescricao("Um reino encantado onde fadas dançam entre flores
+        // e árvores mágicas.");
         // new DestinoDAO().cadastrarDestino(terraDasFadas);
-// new ListarTabelas().listarUsuario();
-// new ListarTabelas().listarUsuarioID("1");
-// new ListarTabelas().listarUsuarioName("ilnara");
-// new ListarTabelas().listarUsuarioEmail("ilnara.ackermann@hotmail.com");
 
+        // testando querys
+        // new ListarTabelas().listarUsuario();
+        // new ListarTabelas().listarUsuarioID("1");
+        // new ListarTabelas().listarUsuarioName("ilnara");
+        // new ListarTabelas().listarUsuarioEmail("ilnara.ackermann@hotmail.com");
 
+        // atualizando o usuario
+        // Usuario ilnara = new Usuario();
+        // ilnara.setCpf("12345678911");
+        // ilnara.setLogradouro("Reino das feras");
+        // ilnara.setNumero(456);
+        // ilnara.setComplemento(null);
+        // ilnara.setCep("12345-698");
+        // ilnara.setBairro("Red Dragon");
+        // ilnara.setCidade("Alada");
+        // ilnara.setEstado("DG");
+        // ilnara.setNome_primeiro("Ilnara");
+        // ilnara.setNome_meio("Ackermann Ribeiro");
+        // ilnara.setNome_ultimo("D Avila");
+        // ilnara.setEmail("ilnara.ackermann@hotmail.com");
+        // ilnara.setSenha("UmaSenhaQu@lqu3r");
+        // chamando a classe UsuarioDao para poder realizar o update no DB
+        // new UsuarioDAO().atualizarUsuario(1, ilnara);
+
+        // VERIFICANDO USUARIO ATUALIZADO
+        // new ListarTabelas().listarUsuarioID("1");
+
+        // Criando outro usuario e adicionando informações
+        // Usuario ilnara = new Usuario();
+        // ilnara.setCpf("12345678911");
+        // ilnara.setLogradouro("Rua da Alegria");
+        // ilnara.setNumero(123);
+        // ilnara.setComplemento(null);
+        // ilnara.setCep("12345-698");
+        // ilnara.setBairro("Magico");
+        // ilnara.setCidade("Encantada");
+        // ilnara.setEstado("OP");
+        // ilnara.setNome_primeiro("Ilnara");
+        // ilnara.setNome_meio("Ackermann Ribeiro");
+        // ilnara.setNome_ultimo("D Avila");
+        // ilnara.setEmail("ilnara.ackermann@hotmail.com");
+        // ilnara.setSenha("UmaSenhaQu@lqu3r");
+        // chamando a classe UsuarioDao para poder realizar o create no DB
+        // new UsuarioDAO().cadastrarUsuario(ilnara);
+
+        // Usuario joao = new Usuario();
+        // joao.setCpf("98765432100");
+        // joao.setLogradouro("Avenida Principal");
+        // joao.setNumero(456);
+        // joao.setComplemento("Bloco A");
+        // joao.setCep("54321-123");
+        // joao.setBairro("Centro");
+        // joao.setCidade("Cidade Nova");
+        // joao.setEstado("MG");
+        // joao.setNome_primeiro("João");
+        // joao.setNome_meio("Silva");
+        // joao.setNome_ultimo("Santos");
+        // joao.setEmail("joao.silva@example.com");
+        // joao.setSenha("OutraSenha123");
+        // new UsuarioDAO().cadastrarUsuario(joao);
+
+        // new ListarTabelas().listarUsuario();
         
+        // new UsuarioDAO().deletarUsuario(2);
         
+        // new ListarTabelas().listarUsuario();
+
     }
 }
--- src/java/JDBC_connect/src/DAO/UsuarioDAO.java
+++ src/java/JDBC_connect/src/DAO/UsuarioDAO.java
@@ -1,5 +1,6 @@
 package DAO;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -7,10 +8,10 @@ import connect.Connect;
 import entity.Usuario;
 
 public class UsuarioDAO {
-    public void cadastrarUsuario (Usuario usuario){
+    public void cadastrarUsuario(Usuario usuario) {
         String sql = "INSERT INTO USUARIO ( CPF, LOGRADOURO, NUMERO, COMPLEMENTO, CEP, BAIRRO, CIDADE, ESTADO, NOME_PRIMEIRO, NOME_MEIO, NOME_ULTIMO, EMAIL, SENHA) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement ps = null;
-        try{
+        try {
             ps = Connect.getConnection().prepareStatement(sql);
             ps.setString(1, usuario.getCpf());
             ps.setString(2, usuario.getLogradouro());
@@ -31,4 +32,56 @@ public class UsuarioDAO {
             e.printStackTrace();
         }
     }
+
+    public void atualizarUsuario(int id, Usuario usuario) {
+        String sql = "UPDATE USUARIO SET CPF = ?, LOGRADOURO = ?, NUMERO = ?, COMPLEMENTO = ?, CEP = ?, BAIRRO = ?, CIDADE = ?, ESTADO = ?, NOME_PRIMEIRO = ?, NOME_MEIO = ?, NOME_ULTIMO = ?, EMAIL = ?, SENHA = ? WHERE ID_USUARIO = ?";
+        try (Connection conn = Connect.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, usuario.getCpf());
+            ps.setString(2, usuario.getLogradouro());
+            ps.setInt(3, usuario.getNumero());
+            ps.setString(4, usuario.getComplemento());
+            ps.setString(5, usuario.getCep());
+            ps.setString(6, usuario.getBairro());
+            ps.setString(7, usuario.getCidade());
+            ps.setString(8, usuario.getEstado());
+            ps.setString(9, usuario.getNome_primeiro());
+            ps.setString(10, usuario.getNome_meio());
+            ps.setString(11, usuario.getNome_ultimo());
+            ps.setString(12, usuario.getEmail());
+            ps.setString(13, usuario.getSenha());
+            ps.setInt(14, id);
+
+            int linhasAfetadas = ps.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                System.out.println("Usuário atualizado com sucesso!");
+            } else {
+                System.out.println("Nenhum usuário foi atualizado.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deletarUsuario(int id) {
+        String sql = "DELETE FROM USUARIO WHERE ID_USUARIO = ?";
+        try (Connection conn = Connect.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+
+            int linhasAfetadas = ps.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                System.out.println("Usuário deletado com sucesso!");
+            } else {
+                System.out.println("Nenhum usuário foi deletado. Verifique o ID.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
