c768853ae1ce353ce11d6de6921924d1df94be2a
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-05 21:58:29 -0300

    feat(compra): Desenvolve entidade Compra - Cria a entidade Compra para representar transações de compra.

---
+++ src/java/api/src/main/java/com/magictrip/api/models/CompraModel.java
@@ -0,0 +1,51 @@
+package com.magictrip.api.models;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "compra")
+public class CompraModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCompra;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", nullable = false)
+    private UsuarioModel usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "idDestino", nullable = false)
+    private DestinoModel destino;
+
+    @ManyToOne
+    @JoinColumn(name = "idVendedor")
+    private VendedorModel vendedor;
+
+    @Temporal(TemporalType.DATE) // Definindo o tipo de temporalidade
+    @Column(name = "data_inicio", nullable = false)
+    private Date dataInicio;
+
+    @Temporal(TemporalType.DATE) // Definindo o tipo de temporalidade
+    @Column(name = "data_fim", nullable = false)
+    private Date dataFim;
+
+    @Column(name = "tipo_pacote", length = 255, nullable = false)
+    private String tipoPacote;
+
+    @Column(name = "quatidade_pessoas", length = 255, nullable = false)
+    private int qunatidadePessoas;
+
+    @Column(name = "valor", length = 255, nullable = false)
+    private Double valor;
+}
