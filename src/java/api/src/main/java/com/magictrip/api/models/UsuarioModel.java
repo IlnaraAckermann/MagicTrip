6b7ff7234cc6e7fe9c9fe69cae4e0d5bde2b43b0
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-10 11:04:05 -0300

    feat(usuario): Adiciona entidade Usuario - Adiciona a modelagem da entidade Usuario que representa os usuários no sistema, incluindo atributos como nome, email e senha.

--- src/java/api/src/main/java/com/magictrip/api/models/UsuarioModel.java
+++ src/java/api/src/main/java/com/magictrip/api/models/UsuarioModel.java
@@ -1,7 +1,9 @@
 package com.magictrip.api.models;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.time.LocalDate;
+
+import org.springframework.hateoas.RepresentationModel;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,16 +11,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
 
+/* Anotações  Lombok para criação de setteres e getters*/
+@Getter
+@Setter
 @Entity
 @Table(name = "usuario")
-public class UsuarioModel implements Serializable {
+public class UsuarioModel extends RepresentationModel<UsuarioModel> implements Serializable {
     private static final long serialVersionUID = 1;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_usuario")
     private Long idUsuario;
 
@@ -26,9 +31,9 @@ public class UsuarioModel implements Serializable {
     private String cpf;
 
     @Column(nullable = false, length = 255)
-    private String endereço;
+    private String endereco;
 
-    @Column(length = 50)
+    @Column(nullable = false, length = 50)
     private String cidade;
 
     @Column(nullable = false, length = 2)
@@ -43,99 +48,8 @@ public class UsuarioModel implements Serializable {
     @Column(nullable = false, length = 100)
     private String senha;
 
-    @Temporal(TemporalType.DATE) // Definindo o tipo de temporalidade para a data de nascimento
     @Column(name = "data_nascimento", nullable = false)
-    private Date dataNascimento;
+    private LocalDate dataNascimento;
 
-    public UsuarioModel(String cpf, String endereço, String cidade, String estado, String nome, String email,
-            String senha, Date dataNascimento) {
-        this.cpf = cpf;
-        this.endereço = endereço;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.dataNascimento = dataNascimento;
-    }
-
-    public UsuarioModel() {
-    }
-
-    public static long getSerialversionuid() {
-        return serialVersionUID;
-    }
-
-    public Long getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(Long idCliente) {
-        this.idUsuario = idCliente;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getEndereço() {
-        return endereço;
-    }
-
-    public void setEndereço(String endereço) {
-        this.endereço = endereço;
-    }
 
 }
