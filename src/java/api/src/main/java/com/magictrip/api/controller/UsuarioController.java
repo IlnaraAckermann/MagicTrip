9df318db73972c591bdbf96db55febadee46448c
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-10 19:30:51 -0300

    feat: Adiciona endpoints CRUD para gerenciamento de usuários
    - Adiciona endpoint POST /usuarios para criar um novo usuário
    - Adiciona endpoint POST /usuarios/list para criar vários usuários
    - Adiciona endpoint GET /usuarios para recuperar todos os usuários
    - Adiciona endpoint GET /usuarios/{id} para recuperar um usuário específico por ID
    - Adiciona endpoint PUT /usuarios/{id} para atualizar um usuário por ID
    - Adiciona endpoint DELETE /usuarios/{id} para excluir um usuário por ID

--- src/java/api/src/main/java/com/magictrip/api/controller/UsuarioController.java
+++ src/java/api/src/main/java/com/magictrip/api/controller/UsuarioController.java
@@ -4,14 +4,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +24,8 @@ import com.magictrip.api.models.UsuarioModel;
 import com.magictrip.api.repositories.UsuarioRepository;
 
 import jakarta.validation.Valid;
+import lombok.val;
+import lombok.var;
 
 @RestController
 public class UsuarioController {
@@ -59,15 +57,13 @@ public class UsuarioController {
 
     /* Métopdo de post, salva um novo usuario */
     @PostMapping("/usuarios")
-    public ResponseEntity<UsuarioModel> saveUsuario(@RequestBody @Valid
-    UsuarioRecordDto usuarioRecordDto) {
-    var usuarioModel = new UsuarioModel();
-    BeanUtils.copyProperties(usuarioRecordDto, usuarioModel);
-    return
-    ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuarioModel));
+    public ResponseEntity<UsuarioModel> saveUsuario(@RequestBody @Valid UsuarioRecordDto usuarioRecordDto) {
+        var usuarioModel = new UsuarioModel();
+        BeanUtils.copyProperties(usuarioRecordDto, usuarioModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuarioModel));
     }
 
-    //nao é possivel fazer a sobrecarga, talvez fazer um novo endpoint
+    // nao é possivel fazer a sobrecarga, talvez fazer um novo endpoint
     /* Métopdo de post, salva vários usuarios */
     @PostMapping("/usuarios/list")
     public ResponseEntity<List<UsuarioModel>> saveUsuario(
@@ -84,16 +80,62 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUsuarios);
     }
 
+    /*
+     * @PutMapping("/usuarios/{id}")
+     * public ResponseEntity<Object> updateUsuario(@PathVariable(value = "id") Long
+     * idUsuario,
+     * 
+     * @RequestBody @Valid UsuarioRecordDto usuarioRecordDto) {
+     * Optional<UsuarioModel> usuario0 = usuarioRepository.findById(idUsuario);
+     * if (usuario0.isEmpty()) { // checks if the user exists
+     * return
+     * ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+     * }
+     * var usuarioModel = usuario0.get();
+     * 
+     * BeanUtils.copyProperties(usuarioRecordDto, usuarioModel);
+     *
+     *
+     * return
+     * ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuarioModel
+     * ));
+     * }
+     */
 
     @PutMapping("/usuarios/{id}")
-    public ResponseEntity<Object> updateUsuario(@PathVariable(value = "id") Long idUsuario,
-            @RequestBody @Valid UsuarioRecordDto usuarioRecordDto) {
+    public ResponseEntity<Object> updateUsuario(@PathVariable(value = "id") long idUsuario,
+            @RequestBody UsuarioRecordDto usuarioRecordDto) {
         Optional<UsuarioModel> usuario0 = usuarioRepository.findById(idUsuario);
         if (usuario0.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário " + idUsuario + " não encontrado.");
         }
         var usuarioModel = usuario0.get();
-        BeanUtils.copyProperties(usuarioRecordDto, usuarioModel);
+
+        if (usuarioRecordDto.cidade() != null) {
+            usuarioModel.setCidade(usuarioRecordDto.cidade());
+        }
+        if (usuarioRecordDto.cpf() != null) {
+            usuarioModel.setCpf(usuarioRecordDto.cpf());
+        }
+        if (usuarioRecordDto.dataNascimento() != null) {
+            usuarioModel.setDataNascimento(usuarioRecordDto.dataNascimento());
+        }
+        if (usuarioRecordDto.email() != null) {
+            usuarioModel.setEmail(usuarioRecordDto.email());
+        }
+        if (usuarioRecordDto.endereco() != null) {
+            usuarioModel.setEndereco(usuarioRecordDto.endereco());
+        }
+        if (usuarioRecordDto.estado() != null)
+            usuarioModel.setEstado(usuarioRecordDto.estado());
+
+        if (usuarioRecordDto.nome() != null) {
+            usuarioModel.setNome(usuarioRecordDto.nome());
+        }
+        if (usuarioRecordDto.senha() != null) {
+            usuarioModel.setSenha(usuarioRecordDto.senha());
+        }
+
         return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuarioModel));
     }
 
