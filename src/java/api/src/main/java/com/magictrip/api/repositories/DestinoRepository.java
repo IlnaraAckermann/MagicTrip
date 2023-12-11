1da2a56a10c22c3324237351d3c7865a18c392c4
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-10 22:53:14 -0300

    Feat: Criada a classe DestinoRepository.java

---
+++ src/java/api/src/main/java/com/magictrip/api/repositories/DestinoRepository.java
@@ -0,0 +1,12 @@
+package com.magictrip.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.magictrip.api.models.DestinoModel;
+
+@Repository
+public interface DestinoRepository extends JpaRepository<DestinoModel, Integer> {
+
+    
+} 
