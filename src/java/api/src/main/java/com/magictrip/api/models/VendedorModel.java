a1d18bf951e28be6f6325f205b5309608908d63e
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-11 22:20:45 -0300

    Fix: Adicionado  extends RepresentationModel<DestinoModel>

--- src/java/api/src/main/java/com/magictrip/api/models/VendedorModel.java
+++ src/java/api/src/main/java/com/magictrip/api/models/VendedorModel.java
@@ -2,6 +2,8 @@ package com.magictrip.api.models;
 
 import java.io.Serializable;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,10 +12,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "vendedor")
-public class VendedorModel implements Serializable {
+public class VendedorModel extends RepresentationModel<VendedorModel> implements Serializable {
   private static final long serialVersionUID = 1;
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
