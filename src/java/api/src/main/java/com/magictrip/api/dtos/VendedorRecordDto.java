618c1e3f01a5579cf12d40d82f3f13d54d5bef7d
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-11 21:13:08 -0300

    Feat: Criado VendedorRecordDto

---
+++ src/java/api/src/main/java/com/magictrip/api/dtos/VendedorRecordDto.java
@@ -0,0 +1,7 @@
+package com.magictrip.api.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record VendedorRecordDto(@NotBlank Long idDUsuario ) {
+    
+}
--- src/java/api/src/main/java/com/magictrip/api/models/VendedorModel.java
+++ src/java/api/src/main/java/com/magictrip/api/models/VendedorModel.java
@@ -16,7 +16,7 @@ import jakarta.persistence.Table;
 public class VendedorModel implements Serializable {
   private static final long serialVersionUID = 1;
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "id_vendedor")
   private int idVendedor;
 
